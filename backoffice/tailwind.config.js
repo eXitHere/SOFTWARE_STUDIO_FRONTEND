b870172b945d10d0d48474cbeaee8230ddb63e6d
Thana <kookza01@gmail.com>
2022-04-08 09:46:34 +0700

    setup login page

--- backoffice/tailwind.config.js
+++ backoffice/tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    // purge: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/styles/**/*.css'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#089BAB',
+                secondary: '#E1F5F6',
+                foreground: '#F1F6F9',
+                background: '#000000',
+            },
+        },
     },
     plugins: [],
 };
